dc6f3b0cb5b639335688ba63f8f35fef0793663e
Alfan Nufi <alfan.s.nufi@gmail.com>
2023-05-18 18:42:31 +0700

    fix: human error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
 
         <title>Meevil</title>
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
@@ -18,7 +17,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased bg-[#0D1117]">
         <div class="min-h-screen" >
