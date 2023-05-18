8400180b0f160852f3f5262322ada69e1ea780cb
Muhammad Nizar Asykary <nizar_as@mhs.unsyiah.ac.id>
2023-05-18 11:17:37 +0700

    navi & sidepane

--- resources/views/layouts/sidepane.blade.php
+++ resources/views/layouts/sidepane.blade.php
@@ -5,7 +5,7 @@
 
     <div class="w-full max-w-md my-4 p-4 rounded shadow sm:p-8 dark:bg-[#161B22] dark:border-gray-700">
         <div class="flex items-center justify-between mb-4">
-            <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Trending Tags</h5>
+            <h5 class="text-white font-bold leading-none text-gray-900 dark:text-white">Trending Tags</h5>
         </div>
         <div>
             <a href="#" class="bg-amber-300 text-gray-800 text-xs font-medium px-2 py-0.5 rounded-full dark:bg-amber-300 dark:hover:bg-amber-500">Ucup</a>
@@ -21,7 +21,7 @@
     
     <div class="w-full max-w-md my-4 p-4 rounded shadow sm:p-8 dark:bg-[#161B22] dark:border-gray-700">
         <div class="flex items-center justify-between mb-4">
-            <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Top Leaderboards</h5>
+            <h5 class="text-white font-bold leading-none text-gray-900 dark:text-white">Top Leaderboards</h5>
             {{-- <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
                 View all
             </a> --}}
@@ -34,11 +34,11 @@
                                 <img class="w-8 h-8 rounded-full object-cover" src="/imgs/testimg.jpg" alt="Neil image">
                             </div>
                             <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Neil Sims
+                                <p class="text-sm font-medium text-yellow-300 truncate dark:text-white">
+                                    Martabak Hambar
                                 </p>
                                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    email@windster.com
+                                    @apaaja 
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
@@ -52,11 +52,11 @@
                                 <img class="w-8 h-8 rounded-full object-cover" src="/imgs/testimg.jpg" alt="Bonnie image">
                             </div>
                             <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Bonnie Green
+                                <p class="text-sm font-medium text-yellow-300 truncate dark:text-white">
+                                    Matcha Velvet
                                 </p>
                                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    email@windster.com
+                                    @pengubahmood
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
@@ -70,11 +70,11 @@
                                 <img class="w-8 h-8 rounded-full object-cover" src="/imgs/testimg.jpg" alt="Michael image">
                             </div>
                             <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Michael Gough
+                                <p class="text-sm font-medium text-yellow-300 truncate dark:text-white">
+                                    Affaan tuh
                                 </p>
                                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    email@windster.com
+                                    @hahahaha
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
